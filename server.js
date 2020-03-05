9e3c81075eacb1eb7250ad9e251387b704d3978c
Uygar98 <38011988+Uygar98@users.noreply.github.com>
2020-03-05 03:43:09 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express.io');
+var app = express();
+app.http().io();
+var PORT = 3000;
+console.log('The server has started' + PORT);
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('index.ejs');
+});
+
+app.io.route('ready', function (req) {
+    req.io.join(req.data)
+    app.io.room(req.data).broadcast('annouce', {
+        message: 'New user has joined the' + req.data + ' room'
+    })
+
+})
+
+
+app.io.route('send', function (req) {
+
+    app.io.room(req.data.room).broadcast('message', {
+        message: req.data.message,
+        author: req.data.author
+    });
+
+})
+
+app.listen(PORT);
